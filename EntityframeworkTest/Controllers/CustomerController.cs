925ed10286f484e61630e0eec8629cee37c87020
Teemu <Teemu@DESKTOP-PK94DMM>
2017-08-11 16:52:27 +0100

    first commit for day 3 code

---
+++ EntityframeworkTest/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using EntityframeworkTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityframeworkTest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index() {
+            return View();
+        }
+
+        public JsonResult GetCustomers() {
+
+            northwindEntities en = new northwindEntities();
+
+            return Json(customers);
+        }
+    }
+}
---
+++ EntityframeworkTest/js/BrowseNorthwind.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $.getJSON("/customer/getcustomers", null, function (json) {
+        var jsondata = JSON.parse(json);
+
+        html = "";
+
+        for (var i = 0; i < jsondata.length; i++) {
+            html += "<tr>" +
+                        "<td>" + jsondata[i].CustomerID + "</td>" +
+                        "<td>" + jsondata[i].CompanyName + "</td>" +
+                        "<td>" + jsondata[i].ContactName + "</td>" +
+                        "<td>" + jsondata[i].ContactTitle + "</td>" +
+                        "<td>" + jsondata[i].Address + "</td>" +
+                        "<td>" + jsondata[i].City + "</td>" +
+                        "<td>" + jsondata[i].Region + "</td>" +
+                        "<td>" + jsondata[i].PostalCode + "</td>" +
+                        "<td>" + jsondata[i].Country + "</td>" +
+                        "<td>" + jsondata[i].Phone + "</td>" +
+                        "<td>" + jsondata[i].Fax + "</td>" +
+                    "</tr>";
+        }
+
+        $(".customertable tbody").html(html);
+    });
+});
